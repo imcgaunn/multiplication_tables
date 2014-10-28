8d4e878962e7bec779dcb3a2a788158f48fa0786
Ian McGaunn <ian_mcgaunn@student.uml.edu>
2014-10-28 14:39:29 +0000

    commit indentation

--- js/gentable.js
+++ js/gentable.js
@@ -6,46 +6,46 @@
 
 // start executing code once the DOM is ready
 $(document).ready(function() {
-  // when the submit event occurs, call this function
-  $("#ranges").submit(function() {
+    // when the submit event occurs, call this function
+    $("#ranges").submit(function() {
 
-    var multiplier_start   = parseInt($("#multiplierStart").val());
-    var multiplier_end     = parseInt($("#multiplierEnd").val());
-    var multiplicand_start = parseInt($("#multiplicandStart").val());
-    var multiplicand_end   = parseInt($("#multiplicandEnd").val());
+        var multiplier_start   = parseInt($("#multiplierStart").val());
+        var multiplier_end     = parseInt($("#multiplierEnd").val());
+        var multiplicand_start = parseInt($("#multiplicandStart").val());
+        var multiplicand_end   = parseInt($("#multiplicandEnd").val());
 
-    // some debugging statements
-    console.log("ier start " + multiplier_start);
-    console.log("ier end " + multiplier_end);
-    console.log("and start " + multiplicand_start);
-    console.log("and end " + multiplicand_end);
+        // some debugging statements
+        console.log("ier start " + multiplier_start);
+        console.log("ier end " + multiplier_end);
+        console.log("and start " + multiplicand_start);
+        console.log("and end " + multiplicand_end);
 
-    // select and clear any table that might already be on page.
-    var table = $("#tbl1 tbody");
-    table.empty();
+        // select and clear any table that might already be on page.
+        var table = $("#tbl1 tbody");
+        table.empty();
 
-    // create a top row
-    table.append($("<tr id='first'>"));
-    $("#first").append($("<td>")); //add blank td
-    for (var i = multiplicand_start; i <= multiplicand_end; i++) {
-      $("#first").append($("<td>").text(i));
-    }
+        // create a top row
+        table.append($("<tr id='first'>"));
+        $("#first").append($("<td>")); //add blank td
+        for (var i = multiplicand_start; i <= multiplicand_end; i++) {
+            $("#first").append($("<td>").text(i));
+        }
 
-    // compute results and insert into table
-    for (var i = multiplier_start; i <= multiplier_end; i++) {
-      var cur_row = $("<tr>");
-      for (var j = multiplicand_start; j <= multiplicand_end; j++) {
-        cur_row.append($("<td>").text(i * j));
-      }
-      table.append(cur_row);
-    }
+        // compute results and insert into table
+        for (var i = multiplier_start; i <= multiplier_end; i++) {
+            var cur_row = $("<tr>");
+            for (var j = multiplicand_start; j <= multiplicand_end; j++) {
+                cur_row.append($("<td>").text(i * j));
+            }
+            table.append(cur_row);
+        }
 
-    // add the column of input values
-    var dropped_first_row = $("#tbl1 tbody tr").slice(1);
-    for (var i = multiplier_start; i <= multiplier_end; i++) {
-      var cur_row = dropped_first_row[i - multiplier_start];
-      $(cur_row).prepend($("<td>").text(i));
-    }
+        // add the column of input values
+        var dropped_first_row = $("#tbl1 tbody tr").slice(1);
+        for (var i = multiplier_start; i <= multiplier_end; i++) {
+            var cur_row = dropped_first_row[i - multiplier_start];
+            $(cur_row).prepend($("<td>").text(i));
+        }
 
-  });
+    });
 });
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // wait until the DOM is completely loaded to start
 // manipulating it.
 $(document).ready(function() {
-  console.log("we've gotten this far");
+    console.log("we've gotten this far");
 });
